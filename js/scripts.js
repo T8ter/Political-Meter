1fb1f09fe8b6b036bc319b7f88d966fb1db49c50
Slater Smith and tate easterbrooks <tate@>
2019-01-17 15:37:59 -0800

    create error message commands in jquery

--- js/scripts.js
+++ js/scripts.js
@@ -11,19 +11,44 @@ $(document).ready(function() {
     var econ = parseInt($("input#economic").val());
     var relig = parseInt($("input#religious").val());
 
-    if (partisan(soc, econ, relig) > 8) {
+
+    if (soc > 5 || soc <= 0 || econ > 5 || econ <= 0 || relig > 5 || relig <= 0) {
+      $("#moderate").hide();
+      $("#conservative").hide();
+      $("#liberal").hide();
+      if (soc > 5 || soc <= 0) {
+        $("#invalid-number").show();
+      }
+      if (econ > 5 || econ <= 0) {
+        $("#invalid-number2").show();
+      }
+      if (relig > 5 || relig <= 0) {
+        $("#invalid-number3").show();
+      }
+    } else if (partisan(soc, econ, relig) > 8) {
       $("#conservative").show();
       $("#liberal").hide();
       $("#moderate").hide();
+      $("#invalid-number").hide();
+      $("#invalid-number2").hide();
+      $("#invalid-number3").hide();
     } else if (partisan(soc, econ, relig) < 8) {
       $("#liberal").show();
       $("#conservative").hide();
       $("#moderate").hide();
+      $("#invalid-number").hide();
+      $("#invalid-number2").hide();
+      $("#invalid-number3").hide();
     } else if (partisan(soc, econ, relig) === 8) {
       $("#moderate").show();
       $("#conservative").hide();
       $("#liberal").hide();
+      $("#invalid-number").hide();
+      $("#invalid-number2").hide();
+      $("#invalid-number3").hide();
     };
+
+
 
 
 
